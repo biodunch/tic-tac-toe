51b0aa7b561882db919a397e411a9e171eb8473c
biodun <biodunch@gmail.com>
2017-06-28 11:54:40 +0100

    Add state to Board

--- src/index.js
+++ src/index.js
@@ -26,8 +26,16 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      squares : Array(9).fill(null)
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i}  />;
+    return <Square value={this.state.squares[i]}  />;
   }
 
   render() {
