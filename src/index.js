a4390d0945597ca80cdabe739d1f7f0eabd1cbca
biodun <biodunch@gmail.com>
2017-06-28 14:18:31 +0100

    Handle Player Change

--- src/index.js
+++ src/index.js
@@ -4,38 +4,37 @@ import './index.css'
 
 
 let value;
-class Square extends React.Component {
-    constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
+function Square (props) {
 
-  handleClick(){
-    this.setState({value:'X'});
-  }
-
-  render() {
     return (
-      <button className="square" onClick={()=>{this.handleClick()}}>
-        {this.state.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
-}
+
 
 class Board extends React.Component {
 
   constructor(props){
     super(props);
     this.state={
-      squares : Array(9).fill(null)
+      squares : Array(9).fill(null),
+      xIsNext : true
     }
   }
 
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : '0';
+    this.setState({
+      squares: squares, 
+      xIsNext : !this.state.xIsNext 
+  });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}  />;
+    return <Square value={this.state.squares[i]}  onClick={() => this.handleClick(i)} />;
   }
 
   render() {
